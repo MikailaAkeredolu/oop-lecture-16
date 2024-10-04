e8162a3e38f20ee7596287b0acb3bd6c26b2a58d
mikailaAKeredolu <makerofappsdotcom@gmail.com>
2024-10-04 14:59:28 -0400

    enums lecture done

--- src/main/java/zoo/enumsdemo/Main.java
+++ src/main/java/zoo/enumsdemo/Main.java
@@ -2,21 +2,37 @@ package zoo.enumsdemo;
 
 public class Main {
     public static void main(String[] args) {
-        Days dayOfTheWeek = Days.FRI;
+        Days dayOfTheWeek = Days.MON;
         Days[] days = Days.values();
 
-        System.out.println(dayOfTheWeek.ordinal());
-        //loops through the days - ordinal
-            Days d = Days.WED;
-            if(d == dayOfTheWeek){
+        switch (dayOfTheWeek){
+            case FRI :
                 System.out.println("Friday Forum");
-            }else if(d == Days.FRI || d == Days.SAT){
-                System.out.println("Yay it's the weekend");
-            }else{
+                break;
+            case SAT, SUN:
+                System.out.println("Closed on weekends");
+                break;
+            default:
                 System.out.println("Just another day");
-            }
+                break;
+        }
 
-            //print every day of the week except friday
+
+        //System.out.println(dayOfTheWeek.ordinal());
+        //loops through the days - ordinal
+//            Days d = Days.WED;
+//            if(d == dayOfTheWeek){
+//                System.out.println("Friday Forum");
+//            }else if(d == Days.FRI || d == Days.SAT){
+//                System.out.println("Yay it's the weekend");
+//            }else{
+//                System.out.println("Just another day");
+//            }
+
+            //print every day of the week except friday -
+
+
+
 
     }
 
